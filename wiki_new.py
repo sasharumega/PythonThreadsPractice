df30ce5a70cd7ff4367906438a827efb9dfcc2b6
sasharumega <92515117+sasharumega@users.noreply.github.com>
2021-11-27 09:48:27 +0500

    Wiki Task Code

---
+++ wiki_new.py
@@ -0,0 +1,31 @@
+import time
+from concurrent.futures import ThreadPoolExecutor
+from urllib.request import Request, urlopen
+import concurrent.futures
+from urllib.parse import unquote
+from tqdm import tqdm
+
+
+def check_link(url):
+    global counter
+    counter += 1
+    print('Checking: ' + format(100 * counter/len(links), '.2f') + '%  Elapsed: ' + format(time.time() - start, '.1f'))
+    try:
+        request = Request(
+            url,
+            headers={'User-Agent': 'Mozilla/5.0 (Windows NT 9.0; Win65; x64; rv:97.0) Gecko/20105107 Firefox/92.0'},
+        )
+        resp = urlopen(request, timeout=5)
+        code = resp.code
+        resp.close()
+    except Exception as e:
+        pass
+
+
+links = open('res.txt', encoding='utf8').read().split('\n')
+
+counter = 0
+start = time.time()
+with ThreadPoolExecutor(max_workers=100) as executor:
+    for _ in executor.map(check_link, links):
+        pass
---
+++ wiki_old.py
@@ -0,0 +1,21 @@
+from urllib.request import Request, urlopen
+import concurrent.futures
+from urllib.parse import unquote
+from tqdm import tqdm
+
+links = open('res.txt', encoding='utf8').read().split('\n')
+
+for i in tqdm(range(len(links))):
+    url = links[i]
+    try:
+        request = Request(
+            url,
+            headers={'User-Agent': 'Mozilla/5.0 (Windows NT 9.0; Win65; x64; rv:97.0) Gecko/20105107 Firefox/92.0'},
+        )
+        resp = urlopen(request, timeout=5)
+        code = resp.code
+        #print(code)
+        resp.close()
+    except Exception as e:
+        #print(url, e)
+        continue
