65f804f773f2c6da0a6ca2f536b1dee8c714af18
sasharumega <92515117+sasharumega@users.noreply.github.com>
2021-12-04 09:10:46 +0500

    money upload

---
+++ money_new.py
@@ -0,0 +1,24 @@
+from hashlib import md5
+import time
+from concurrent.futures import ProcessPoolExecutor
+from random import choice
+
+def run_one_step():
+	while True:
+		s = "".join([choice("0123456789") for i in range(50)])
+		h = md5(s.encode('utf8')).hexdigest()
+		if h.endswith("000"):
+			break
+	print(s, h)
+	return s, h
+
+
+if __name__ == '__main__':
+
+	start = time.time()
+
+	with ProcessPoolExecutor(max_workers=4) as executor:
+		for i in range(30):
+			executor.submit(run_one_step)
+
+	print('Time: ' + format(time.time() - start, '.1f'))
---
+++ money_old.py
@@ -0,0 +1,15 @@
+from hashlib import md5
+import time
+from random import choice
+
+counter = 0
+start = time.time()
+
+while counter < 30:
+    s = "".join([choice("0123456789") for i in range(50)])
+    h = md5(s.encode('utf8')).hexdigest()
+
+    if h.endswith("228"):
+        print(s, h)
+        counter += 1
+print('Time: ' + format(time.time() - start, '.1f'))
